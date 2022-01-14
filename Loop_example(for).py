73e9db0d5bfc4212259ee08fc23dc8f8d9fa1bed
vitorio97 <lostcanaria@gmail.com>
2022-01-14 21:41:40 +0700

    # for and while loop

---
+++ Loop_example(for).py
@@ -0,0 +1,11 @@
+# Test
+# Test2
+
+book_quantity = 10
+print("Teacher said, 'read all books which are available here'")
+read_book = 0
+
+for book_quantity in range (1,book_quantity+1):
+    print(f"Book - {book_quantity} has been read")
+
+print(f"Book read = {book_quantity}")
---
+++ While(loop).py
@@ -0,0 +1,12 @@
+book_sum=200
+print("Teacher said, 'read all books'")
+
+book_read=0
+print(f"No. book read {book_read}")
+
+while book_read<book_sum:
+    book_read = book_read + 1
+    print(f"Book - {book_read} has been read")
+
+
+print(f'Numbers of book already read {book_read}')
